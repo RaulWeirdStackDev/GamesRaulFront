b4f72f0e3396b8d0caf98acae9890d26b2bf6dda
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-08-17 16:10:02 -0400

    arreglando puerto en Login

--- src/views/PublicViews/Login/Login.jsx
+++ src/views/PublicViews/Login/Login.jsx
@@ -15,7 +15,7 @@ export const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/users/login", {
+      const response = await fetch("http://localhost:3007/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
