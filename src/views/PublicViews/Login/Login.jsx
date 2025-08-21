9d0f64511ede2bbf834542370ec8263eb42b462e
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-08-21 19:21:58 -0400

    modularizando login en backend

--- src/views/PublicViews/Login/Login.jsx
+++ src/views/PublicViews/Login/Login.jsx
@@ -20,7 +20,7 @@ export const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3007/api/users/login", {
+      const response = await fetch("http://localhost:3007/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
